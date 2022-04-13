f4bbc21d6729a1cd6e683013cfff9e48a0d865dd
angelDariaux <angeldariaux@gmail.com>
2022-04-12 21:10:30 -0600

    mejorada la solucion del problema 8 de invertir un numero

--- JavaScript/08invertirNumero.js
+++ JavaScript/08invertirNumero.js
@@ -1,31 +1,46 @@
-console.group('Mi respuesta:');
+    // Funcion para convertir en string el numero y luego invertirlo
+
+let invertido;
+const reverse = (palabra) => { 
+  invertido = parseInt(palabra.split("").reverse().join(""));
+}
+
+    // Codigo que ejecuta el trabajo: 
+
 const invertirNumero = (numero) => { 
   if (numero >= 0) { 
     let texto = numero.toString();
-    let invertido;
-    const reverse = (palabra) => { 
-      invertido = palabra.split("").reverse().join("");
-    }
+
     reverse(texto)
     console.log(`Numero original: ${numero}, invertido es: ${invertido}`)
-
   } else { 
     let texto = (numero * -1).toString();
-    let invertido;
-    const reverse = (palabra) => { 
-      invertido = palabra.split("").reverse().join("");
-    }
+
     reverse(texto)
     console.log(`Numero original: ${numero}, invertido es: -${invertido}`)
   }
 }
 
 invertirNumero(-52);
-console.groupEnd();
-
-
-console.group('Respuesta del profe:');
 
 
 
-console.groupEnd();
+console.groupEnd('Otra forma mas limpia: ');
+
+const reverseNumber = (numero) => { 
+
+  let invertido = parseInt(
+                    numero
+                    .toString() // pasa a string el numero
+                    .split('') // separa en arrays el string
+                    .reverse() // invierte el orden del array
+                    .join('') // junta un array. 
+                  ) * Math.sign(numero) // ? Muestra el signo del numero
+
+    return invertido;
+}
+
+console.log(`El numero invertido es: ${reverseNumber(-78)}`)
+
+
+console.group();
